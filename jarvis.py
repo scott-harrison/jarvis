3b349458fc6a9f0995b4cd361ecaa9e1452c705c
Toruk <scotth142@googlemail.com>
2021-08-31 21:03:22 +0100

    added greeting to jarvis

--- jarvis.py
+++ jarvis.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from ai import AI
 
 jarvis = AI()
@@ -10,3 +11,16 @@ while True and command != "goodbye":
 
     if command in ["hello", "hi"]:
         jarvis.say("Hello sir, how can I be of assistance?")
+    if command in ["morning", "afternoon", "evening"]:
+        now = datetime.now()
+        hr = now.hour
+        if hr <= 0 <=12:
+            message = "Morning"
+        if hr >=12 <= 17:
+            message = "Afternoon"
+        if hr >=17 <=21:
+            message = "Evening"
+        if hr > 21: message = "Night"
+
+        message = "Good " + message + " sir"
+        jarvis.say(message)
